7c9bff2e7863ff1c2d75e4ccff733e3db3f1d68c
Vanlt4s <herviamoyamatias@gmail.com>
2022-11-12 15:45:58 -0300

    Menus Parte Final
    .- Añadida pantalla de selección de nivel
    .- Mejora de Interfeaces de nuevo
    .- Orden de Carpetas
    .- Nuevos Assets Agregados

--- Assets/Scripts/SceneS.cs
+++ Assets/Scripts/SceneS.cs
@@ -19,7 +19,21 @@ public class SceneS : MonoBehaviour
 
     public void MoverEscenas()
     {
-        SceneManager.LoadScene("EscenaTD");
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void CargarTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void CargarLevel1()
+    {
+        SceneManager.LoadScene("nivel1");
+    }
+
+    public void Cargarlevel2()
+    {
+        SceneManager.LoadScene("nivel2");
     }
 
     public void Reiniciar()
